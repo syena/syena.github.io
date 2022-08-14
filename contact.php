a7bcbd16ab68b47c4379c342073b9e6143340e80
syena <yasmine_messaoudi@hotmail.com>
2022-08-14 16:51:28 +0400

    commit git

---
+++ contact.php
@@ -0,0 +1,29 @@
+<?php
+
+function strip_crlf($string)
+{
+    return str_replace("\r\n", "", $string);
+}
+
+if (! empty($_POST["send"])) {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $content = $_POST["message"];
+
+    $toEmail = "yasmine_messaoudi@hotmail.com";
+    // CRLF Injection attack protection
+    $name = strip_crlf($name);
+    $email = strip_crlf($email);
+    if (! filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "The email address is invalid.";
+    } else {
+        // appending \r\n at the end of mailheaders for end
+        $mailHeaders = "From: " . $name . "<" . $email . ">\r\n";
+        if (mail($toEmail, $subject, $content, $mailHeaders)) {
+            $message = "Your contact information is received successfully.";
+            $type = "success";
+        }
+    }
+}
+require_once "contact.php";
+?>
